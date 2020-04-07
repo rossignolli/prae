c59da4f601f7cf3f933a2af3266ddff0dbef8482
Vitor <vitorrossignolli@gmail.com>
2020-04-07 20:40:46 -0300

    Logon Page CSS DONE
    JSX and CSS Done.
    
    Some fixes to come.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,32 +1,46 @@
-import React from 'react';
+import React from "react";
+import { FiMail, FiLock } from "react-icons/fi";
+
 //import {} from 'react-icons/fi'
 
-import './styles.css'
-import login_img from '../../assets/images/login_image.png'
+import "./styles.css";
+import login_img from "../../assets/images/login_image.png";
 
-export default  function Logon (){
-      return (
-            <div className="logon-container">
-
-                  <div className="content">
-                  <section className="form">
-                        <form action="">
-                              <h1>Bem vindo ao Prae</h1>
-                              <input placeholder="Seu email"/>
-                              <input placeholder="Senha"/>
-                              <button type="submit">Entrar</button>
-                              <a href="/requireaccount">
-                                    Solicite uma conta
-                              </a>
-                        </form>
-                        
-                  </section>
-                  <div className="adv">
-                  <img src={login_img} alt="Login"/>
-                  <h3>Auditorias e processos de qualidade</h3>
-                  <p>Prae é para todos os gestores de T.I que desejam controlar preventivas de forma inovadora.</p>
-                  </div>
-                  </div>
+export default function Logon() {
+  return (
+    <div className="logon-container">
+      <div className="content">
+        <div className="form">
+          <h1>Bem vindo ao Prae</h1>
+          <form action="">
+            <div className="input-container">
+              <i><FiMail size={18} color="#612f74" /></i>
+              <input placeholder="Seu email" />
             </div>
-      );
+
+            <div className="input-container">
+            <FiLock size={20} color="#612f74" />
+            <input placeholder="Senha" />
+            </div>
+            
+           
+            <button type="submit">Entrar</button>
+            <p> Não tem uma conta? </p>
+            <a href="/requireaccount">Solicite uma conta</a>
+          </form>
+        </div>
+
+        <div className="adv">
+          <img src={login_img} alt="Login" />
+          <p className="ptitle">Auditorias e processos de qualidade</p>
+          <p>
+            {" "}
+            <span>Prae</span> é para todos os gestores de T.I <br /> que desejam
+            controlar preventivas
+            <br /> de forma inovadora.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
