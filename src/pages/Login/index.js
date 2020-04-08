f7cf9107c0558da292cac2bf26c90b27b9eb2291
Vitor <vitorrossignolli@gmail.com>
2020-04-08 12:10:28 -0300

    ## BugFix | 06/04/1994 |  Responsive Fix |  Cleanup Files

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -9,6 +9,7 @@ import login_img from "../../assets/images/login_image.png";
 export default function Logon() {
   return (
     <div className="logon-container">
+
       <div className="content">
         <div className="form">
           <h1>Bem vindo ao Prae</h1>
@@ -34,13 +35,15 @@ export default function Logon() {
           <img src={login_img} alt="Login" />
           <p className="ptitle">Auditorias e processos de qualidade</p>
           <p>
-            {" "}
             <span>Prae</span> é para todos os gestores de T.I <br /> que desejam
             controlar preventivas
             <br /> de forma inovadora.
           </p>
         </div>
       </div>
+
+      
+<footer><a href="123">Copyright © 2020 - Prae Team </a></footer>
     </div>
   );
 }
